be55439fa0ba8309b99652a14c8c2c9b45897d4c
Kawaljeet Kaur <kawal.matharu2851@gmail.com>
2019-01-31 12:17:52 -0800

    Add files via upload

--- stack.py
+++ stack.py
@@ -8,15 +8,23 @@ class stack():
     def pop(self):
         return self.items.pop()
 
+    def isEmpty(self):
+        return self.items == []
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.items[-1]
+
     def get_stack(self):
         return self.items
 
 
 s = stack()
+# print(s.isEmpty())
 s.push("100")
 s.push("200")
-print(s.get_stack())
 s.push("300")
+s.push("400")
+print(s.isEmpty())
 print(s.get_stack())
-s.pop()
-print(s.get_stack())
+print(s.peek())
